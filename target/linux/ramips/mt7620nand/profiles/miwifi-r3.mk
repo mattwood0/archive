c367b15589bb6100135c9275d03ad4240bf6c0f1
mattwood0 <matt@mntn.co.uk>
2018-10-18 22:40:57 +0100

    Create miwifi-r3.mk
    Xiaomi Mi Router 3 support

---
+++ target/linux/ramips/mt7620nand/profiles/miwifi-r3.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/MIWIFI-R3
+ NAME:=Xiaomi MiWiFi R3
+ PACKAGES:=\
+	kmod-usb2 kmod-usb-ohci uboot-envtools
+endef
+
+define Profile/MIWIFI-R3/Description
+ Support for Xiaomi MiWiFi R3 routers
+endef
+$(eval $(call Profile,MIWIFI-R3))
